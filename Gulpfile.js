e28825a1993ee4fd36bc476e0ddd48c19f89f092
Michael Joyner <joyner.michael@gmail.com>
2014-08-12 09:42:39 +0800

    package.json

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,3 +1,21 @@
-/**
- * Created by user on 8/7/2014.
- */
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    rename = require('gulp-rename'),
+    minifycss = require('gulp-minify-css');
+
+gulp.task('sass', function () {
+    return gulp.src('app/assets/sass/**/*.scss')
+        .pipe(sass({style: 'expanded'}))
+        .pipe(autoprefixer('last 5 version'))
+        .pipe(gulp.dest('public/styles/css'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifycss())
+        .pipe(gulp.dest('public/styles/css'));
+});
+
+gulp.task('watch', function(){
+    return gulp.watch('app/assets/sass/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass', 'watch']);
